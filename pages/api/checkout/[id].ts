7a72755d0689aecd3b1159fc7c5e09698550c4ea
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-10-19 13:44:27 +0200

    Update redirect and checkout result

---
+++ pages/api/checkout/[id].ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Stripe from 'stripe';
+const secrete_key: any = process.env.STRIPE_SECRET_KEY
+const stripe = new Stripe(secrete_key, {
+    apiVersion: '2023-10-16',
+});
+
+const getCheckoutSession = async(req: NextApiRequest, res: NextApiResponse) => {
+    const { id } = req.query;
+    const session = await stripe.checkout.sessions.retrieve(id as string)
+    res.status(200).json({ session })
+}
+
+export default getCheckoutSession;
