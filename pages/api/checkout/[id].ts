66d258ebed7d5f213ac7a220b488143fde783d16
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-10-19 15:50:20 +0200

    Update [id].ts

--- pages/api/checkout/[id].ts
+++ pages/api/checkout/[id].ts
@@ -7,7 +7,7 @@ const stripe = new Stripe(secrete_key, {
 
 const getCheckoutSession = async(req: NextApiRequest, res: NextApiResponse) => {
     const { id } = req.query;
-    const session = await stripe.checkout.sessions.retrieve(id as string)
+    const session = await stripe.checkout.sessions.retrieve(id as string, {expand: ['payment_intent']})
     res.status(200).json({ session })
 }
 
